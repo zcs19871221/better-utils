d60c2edc5645fc157ece502465259d7b2565b631
zcs <530627552@qq.com>
2020-07-08 14:25:58 +0800

    optimze queue

--- src/data_structure/prioritiy_queue.test.ts
+++ src/data_structure/prioritiy_queue.test.ts
@@ -1,42 +1,43 @@
 import PriorityQueue from './priority_queue';
 
 it('priority queue', () => {
-    const list = [9, 8, 7, 10, 3, 6, 5, 4, 2, 11]
-    const q = new PriorityQueue<number>(list)
-    expect(q['stack']).toEqual([2, 3, 5, 4, 9, 8, 6, 10, 7, 11])
-    expect(q.peek()).toBe(2);
-    expect(q.poll()).toBe(2);
-    expect(q.poll()).toBe(3);
-    expect(q.poll()).toBe(4);
-    expect(q.poll()).toBe(5);
-    q.add(1)
-    expect(q.poll()).toBe(1);
-    expect(q.poll()).toBe(6);
-    expect(q.poll()).toBe(7);
-    expect(q.poll()).toBe(8);
-    expect(q.poll()).toBe(9);
-    expect(q.poll()).toBe(10);
-    expect(q.isEmpty()).toBe(false)
-    expect(q.poll()).toBe(11);
-    expect(q.poll()).toBe(null);
-    expect(q.poll()).toBe(null);
-    expect(q.isEmpty()).toBe(true);
-})
+  const list = [9, 8, 7, 10, 3, 6, 5, 4, 2, 11];
+  const q = new PriorityQueue<number>(list);
+  expect(q['stack']).toEqual([2, 3, 5, 4, 9, 8, 6, 10, 7, 11]);
+  expect(q.peek()).toBe(2);
+  expect(q.poll()).toBe(2);
+  expect(q.poll()).toBe(3);
+  expect(q.poll()).toBe(4);
+  expect(q.poll()).toBe(5);
+  q.add(1);
+  expect(q.poll()).toBe(1);
+  expect(q.poll()).toBe(6);
+  expect(q.poll()).toBe(7);
+  expect(q.poll()).toBe(8);
+  expect(q.poll()).toBe(9);
+  expect(q.poll()).toBe(10);
+  expect(q.isEmpty()).toBe(false);
+  expect(q.poll()).toBe(11);
+  expect(q.poll()).toBe(null);
+  expect(q.poll()).toBe(null);
+  expect(q.isEmpty()).toBe(true);
+});
 
 it('priority queue remove', () => {
-    const list = [1, 100, 3, 105, 106, 7, 8, 200, 201, 202, 203, 9]
-    const q = new PriorityQueue<number>(list)
-    q.remove(105);
-    expect(q['stack']).toEqual([1, 9, 3, 100, 106, 7, 8, 200, 201, 202, 203])
-
-})
+  const list = [1, 100, 3, 105, 106, 7, 8, 200, 201, 202, 203, 9];
+  const q = new PriorityQueue<number>(list);
+  q.remove(105);
+  expect(q['stack']).toEqual([1, 9, 3, 100, 106, 7, 8, 200, 201, 202, 203]);
+});
 it('priority comparator', () => {
-    const list = [-1, 4, 6, 8, -4, 6, -6, 3, -2, 3, -3, -8]
-    const q = new PriorityQueue<number>(list, (a, b) => Math.abs(b) - Math.abs(a) )
-    const result = []
-    while (!q.isEmpty()) {
-        result.push(q.poll())
-    }
-    expect(result).toEqual([8, -8, 6, 6, -6,-4,  4, 3, 3, -3, -2, -1])
-
-})
+  const list = [-1, 4, 6, 8, -4, 6, -6, 3, -2, 3, -3, -8];
+  const q = new PriorityQueue<number>(
+    list,
+    (a, b) => Math.abs(b) - Math.abs(a),
+  );
+  const result = [];
+  while (!q.isEmpty()) {
+    result.push(q.poll());
+  }
+  expect(result).toEqual([8, -8, 6, 6, -6, -4, 4, 3, -3, 3, -2, -1]);
+});
--- src/data_structure/priority_queue.ts
+++ src/data_structure/priority_queue.ts
@@ -1,133 +1,130 @@
 interface PriorityQueue<E> {
-    add(e: E): boolean;
-    clear(): void;
-    contains(e: E): boolean;
-    peek(): E | null;
-    poll(): E | null;
-    remove(e: E): boolean;
-    size: number;
-    isEmpty(): boolean;
+  add(e: E): boolean;
+  clear(): void;
+  contains(e: E): boolean;
+  peek(): E | null;
+  poll(): E | null;
+  remove(e: E): boolean;
+  size: number;
+  isEmpty(): boolean;
 }
 
 interface Comparator<E> {
-    (a: E, b: E): number;
+  (a: E, b: E): number;
 }
-
-
 
 export default class PriorityQueueImpl<E> implements PriorityQueue<E> {
-    private stack: E[] = [];
-    private comparator: Comparator<E>;
-    size: number = 0;
-    constructor();
-    constructor(comparator: Comparator<E>);
-    constructor(list: E[]);
-    constructor(list: E[], comparator?: Comparator<E>);
-    constructor(listOrComparator?: E[] | Comparator<E>, comparator?: Comparator<E> ) {
-        this.comparator = <any>PriorityQueueImpl.defaultCompartor;
-        if (Array.isArray(listOrComparator)) {
-            if (comparator !== undefined) {
-                this.comparator = comparator
-            }
-            listOrComparator.forEach(e => {
-                this.add(e)
-            })
-        } else if (typeof listOrComparator === 'function') {
-            this.comparator = listOrComparator;
-        }
+  private stack: E[] = [];
+  private comparator: Comparator<E>;
+  size: number = 0;
+  constructor();
+  constructor(comparator: Comparator<E>);
+  constructor(list: E[]);
+  constructor(list: E[], comparator?: Comparator<E>);
+  constructor(
+    listOrComparator?: E[] | Comparator<E>,
+    comparator?: Comparator<E>,
+  ) {
+    this.comparator = <any>PriorityQueueImpl.defaultCompartor;
+    if (Array.isArray(listOrComparator)) {
+      if (comparator !== undefined) {
+        this.comparator = comparator;
+      }
+      listOrComparator.forEach(e => {
+        this.add(e);
+      });
+      console.log(this.stack);
+    } else if (typeof listOrComparator === 'function') {
+      this.comparator = listOrComparator;
     }
+  }
 
-    static defaultCompartor: Comparator<number> = (a: number, b: number) => a - b;
+  static defaultCompartor: Comparator<number> = (a: number, b: number) => a - b;
 
+  add(e: E) {
+    this.size += 1;
+    this.slipUp(this.size - 1, e);
+    return true;
+  }
 
-    add(e: E) {
-        this.size += 1;
-        this.slipUp(this.size - 1, e)
-        return true;
+  remove(e: E) {
+    const pos = this.stack.indexOf(e);
+    if (pos > -1) {
+      const last = this.stack[this.size - 1];
+      this.size -= 1;
+      this.stack.length = this.size;
+      this.slipDown(pos, last);
+      if (this.stack[pos] === last) {
+        this.slipUp(pos, last);
+      }
+      return true;
     }
+    return false;
+  }
 
-    remove(e: E) {
-        const pos = this.stack.indexOf(e)
-        if (pos > -1) {
-            const last = this.stack[this.size - 1];
-            this.size -= 1;
-            this.stack.length = this.size;
-            this.slipDown(pos, last)
-            if (this.stack[pos] === last) {
-                this.slipUp(pos, last)
-            }
-            return true;
-        }
-        return false;
+  poll() {
+    if (this.size === 0) {
+      return null;
     }
-
-    poll() {
-        if (this.size === 0) {
-            return null;
-        }
-        const e = this.stack[0];
-        const last = this.stack[this.size - 1]
-        this.size -= 1;
-        this.stack.length = this.size;
-        if (this.size !== 0) {
-            this.slipDown(0, last)
-        }
-        return e;
+    const e = this.stack[0];
+    const last = this.stack[this.size - 1];
+    this.size -= 1;
+    this.stack.length = this.size;
+    if (this.size !== 0) {
+      this.slipDown(0, last);
     }
+    return e;
+  }
 
+  clear() {
+    this.size = 0;
+    this.stack = [];
+  }
 
-    clear() {
-        this.size = 0;
-        this.stack = []
+  contains(e: E) {
+    return this.stack.includes(e);
+  }
+
+  peek() {
+    if (this.size === 0) {
+      return null;
     }
+    return this.stack[0];
+  }
 
-  
+  isEmpty() {
+    return this.size === 0;
+  }
 
-    contains(e: E) {
-        return this.stack.includes(e)
+  private slipUp(index: number, e: E) {
+    while (index > 0) {
+      const parentIndex = (index - (index % 2 === 0 ? 2 : 1)) / 2;
+      const parent = this.stack[parentIndex];
+      if (this.comparator(parent, e) <= 0) {
+        break;
+      }
+      this.stack[index] = parent;
+      index = parentIndex;
     }
+    this.stack[index] = e;
+  }
 
-    peek() {
-        if (this.size === 0) {
-            return null;
-        }
-        return this.stack[0];
+  private slipDown(index: number, e: E) {
+    while (index < Math.floor(this.size / 2)) {
+      let child = index * 2 + 1;
+      const right = child + 1;
+      if (
+        right < this.size &&
+        this.comparator(this.stack[right], this.stack[child]) < 0
+      ) {
+        child = right;
+      }
+      if (this.comparator(e, this.stack[child]) <= 0) {
+        break;
+      }
+      this.stack[index] = this.stack[child];
+      index = child;
     }
-
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    private slipUp(index: number, e: E) {
-        while (index > 0) {
-            const parentIndex = (index - (index % 2 === 0 ? 2 : 1)) / 2
-            const parent = this.stack[parentIndex]
-            if (this.comparator(e, parent) >= 0) {
-                break;
-            }
-            this.stack[index] = parent;
-            index = parentIndex;
-        }
-        this.stack[index] = e;
-    }
-
-
-
-    private slipDown(index: number, e: E) {
-        while (index < Math.floor(this.size / 2)) {
-            let child = index * 2 + 1;
-            const right = child + 1;
-            if (right < this.size && this.comparator(this.stack[child], this.stack[right]) > 0) {
-                child = right;
-            }
-            if (this.comparator(e, this.stack[child]) < 0) {
-                break;
-            }
-            this.stack[index] = this.stack[child];
-            index = child;
-        }
-        this.stack[index] = e;
-    }
-
+    this.stack[index] = e;
+  }
 }
-
